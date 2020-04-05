8523b4f20e2a772425dc40ccfb1544f5d6c480da
ch00226855 <ch00226855@gmail.com>
2020-04-04 20:52:39 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app = Flask(__name__)
 # Check https://keras.io/applications/
 from tensorflow.keras.applications.resnet50 import ResNet50
 model = ResNet50(weights='imagenet')
-model.save('')
+#model.save('')
 print('Model loaded. Check http://127.0.0.1:5000/')
 
 
